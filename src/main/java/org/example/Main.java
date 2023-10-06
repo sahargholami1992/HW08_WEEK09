3c6442e268290122adb04d8d191a081700f567ea
sahar_gholami <sahar_gholami03@gmail.com>
2023-10-06 21:35:32 +0330

    call menu

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.example.menu.PublicMenu;
+
+
+public class Main {
+    public static void main(String[] args) {
+        PublicMenu publicMenu = new PublicMenu();
+        publicMenu.publicMenu();
+    }
+}
